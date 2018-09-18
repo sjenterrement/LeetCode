5d32efa3ddeb3417cf456a4c80da2bc0397215df
sjenterrement <sjenterrement@gmail.com>
2018-09-18 22:54:26 +0800

    2018.9.18...leetcode

--- src/isValid/IsValid.java
+++ src/isValid/IsValid.java
@@ -4,29 +4,28 @@ import java.util.Stack;
 
 public class IsValid {
 	public static void main(String[] args) {
-		int sign=0;
-		String s="(){}[";
-		Stack<Character> stack=new Stack<Character>();
-		for (char c : s.toCharArray()) {
-//			System.out.println(c);
-			if (c=='(') {
-				System.out.println("jj");
-				stack.push(')');
-			}
-			else if (c=='[') {
-				stack.push(']');
-			}
-			else if (c=='{') {
-				stack.push('}');
-			}
-			else if (s.isEmpty()||stack.pop()!=c) {
-				int sign1=1;
-				System.out.println(sign1);
-			}
-		}
-		
-//		System.out.println(sign);
-		
-		System.out.println(sign==0);
+
+		String s = "(){[";
+		System.out.println(isValid(s));
+
 	}
+	
+	public static boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                stack.push(')');
+            }
+            else if(c=='['){
+                stack.push(']');
+            }
+            else if(c=='{'){
+                stack.push('}');
+            }
+            else if(stack.isEmpty()||stack.pop()!=c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
