2aa1f2d8b7ff35bdb7caf58006456989266ba095
sjenterrement <sjenterrement@gmail.com>
2018-09-26 22:33:43 +0800

    2018.9.26...

--- src/removeDuplicates/Solution.java
+++ src/removeDuplicates/Solution.java
@@ -1,25 +1,23 @@
 package removeDuplicates;
 
 public class Solution {
-	
+
 	public static void main(String[] args) {
-		int[] s= {
-				1,2,3,3,4,4
-		};
+		int[] s = { 1, 2, 3, 3, 4, 4 };
 		System.out.println(removeDuplicates(s));
 	}
-	
+
 	public static int removeDuplicates(int[] nums) {
-		
-		int l=nums.length;
-		
-		for (int i = 0; i < nums.length-1; i++) {
-			for (int j = i+1; j < nums.length; j++) {
-				if (nums[i]==nums[j]) {
-					l--;
-				}
+
+		if (nums.length == 0)
+			return 0;
+		int i = 0;
+		for (int j = 1; j < nums.length; j++) {
+			if (nums[j] != nums[i]) {
+				i++;
+				nums[i] = nums[j];
 			}
 		}
-		return l;
+		return i + 1;
 	}
 }
