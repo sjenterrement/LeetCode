c3b4a622f8922751d8b6f6a097be053fca1baad6
sjenterrement <sjenterrement@gmail.com>
2018-09-29 22:00:57 +0800

    2018.9.29...remvoeElment.pt2

--- src/removeElement/Solution.java
+++ src/removeElement/Solution.java
@@ -2,25 +2,34 @@ package removeElement;
 
 public class Solution {
 	public static int removeElement(int[] nums, int val) {
-		int l = nums.length;
-		for (int i = 0; i < nums.length-1; i++) {
-			if (nums[i] == val) {
-				
-				for (int j = i; j < nums.length - 2; j++) {
-					nums[j] = nums[j + 1];
-					
-				}
-				l--;
-				i--;	
-			}
-		}
+//		int l = nums.length;
+//		for (int i = 0; i < nums.length-1; i++) {
+//			if (nums[i] == val) {
+//				
+//				for (int j = i; j < nums.length - 2; j++) {
+//					nums[j] = nums[j + 1];
+//					
+//				}
+//				l--;
+//				i--;	
+//			}
+//		}
+//		
+//		if (nums[nums.length-1]==val) {
+//			nums.toString().substring(0, nums.length-1);
+//			l--;
+//		}
+//
+//		return l;
 		
-		if (nums[nums.length-1]==val) {
-			nums.toString().substring(0, nums.length-1);
-			l--;
-		}
-
-		return l;
+		int i = 0;
+	    for (int j = 0; j < nums.length; j++) {
+	        if (nums[j] != val) {
+	            nums[i] = nums[j];
+	            i++;
+	        }
+	    }
+	    return i;
 	}
 
 	public static void main(String[] args) {
